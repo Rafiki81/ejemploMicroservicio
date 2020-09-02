a3e848b6a3c9b955338d47ce815cb0f17afd9a43
rpere <rpere@CASA-DESKTOP.home>
2020-09-02 22:59:40 +0200

    Arreglado Controller GET

--- ejemplo/src/main/java/com/rafa/ejemplo/controllers/PersonaController.java
+++ ejemplo/src/main/java/com/rafa/ejemplo/controllers/PersonaController.java
@@ -1,9 +1,8 @@
 package com.rafa.ejemplo.controllers;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,7 @@ public class PersonaController {
 	PersonaSevice personaService;
 	
 	@GetMapping("/{dni}")
-	public Persona getPersona(@PathParam(value = "dni") String dni) {
+	public Persona getPersona(@PathVariable String dni) {
 		
 		return personaService.obtenerPersonaPorId(dni);
 		
